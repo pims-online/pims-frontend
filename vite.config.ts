fbfe2dcf519dc024644a7bd5e934c543dd62a062
David Bretaud <david.bretaud.dev@gmail.com>
2024-06-30 17:21:15 +0200

    feat(widget): Create a config to make the App as a widget

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
-// https://vitejs.dev/config/
+const URL_CDN = "https://pims-frontend.vercel.app/";
+const URL_LOCALHOST = "http://localhost:4173/";
+const buildLocal = true;
+const URL = buildLocal ? URL_LOCALHOST : URL_CDN;
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), cssInjectedByJsPlugin()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        widget: "./src/widget.tsx",
+      },
+      output: {
+        entryFileNames: (assetInfo) => {
+          return assetInfo.name === "widget"
+            ? "assets/js/[name].js"
+            : "assets/[name].js";
+        },
+      },
+    },
+  },
+  experimental: {
+    renderBuiltUrl(filename: string) {
+      return URL + filename;
+    },
+  },
+});
+
+// TUTO
