e5a18cf62d96c05a1091c7e9b85363fc58e8a415
David Bretaud <david.bretaud.dev@gmail.com>
2024-07-14 13:03:55 +0200

    feat(dsfr-provider): Implement a DSFR wrapper to integrate DSFR in site and widget

---
+++ src/DSFRWrapper.tsx
@@ -0,0 +1,62 @@
+import { startReactDsfr } from '@codegouvfr/react-dsfr/spa';
+
+function loadAssets(cdn_url?: string) {
+	// When local or within the build, we don't need to pass the CDN URL, as it is in the same root directory.
+	// This is required only for the Widget, that is implemented in another site, thus need to load assets from the cdn
+	const CDN_URL = cdn_url || '';
+
+	// REMOVE favicon if widget !!!
+
+	const links = [
+		{
+			rel: 'apple-touch-icon',
+			href: `${CDN_URL}/dsfr/favicon/apple-touch-icon.png`,
+		},
+		{
+			rel: 'icon',
+			href: `${CDN_URL}/dsfr/favicon/favicon.svg`,
+			type: 'image/svg+xml',
+		},
+		{
+			rel: 'shortcut icon',
+			href: `${CDN_URL}/dsfr/favicon/favicon.ico`,
+			type: 'image/x-icon',
+		},
+		{
+			rel: 'manifest',
+			href: `${CDN_URL}/dsfr/favicon/manifest.webmanifest`,
+			crossOrigin: 'use-credentials',
+		},
+		{
+			rel: 'stylesheet',
+			href: `${CDN_URL}/dsfr/utility/icons/icons.min.css`,
+		},
+		{
+			rel: 'stylesheet',
+			href: `${CDN_URL}/dsfr/dsfr.min.css`,
+		},
+	];
+
+	links.forEach((linkData) => {
+		const link = document.createElement('link');
+		Object.keys(linkData).forEach((attr) => {
+			// @ts-expect-error => linkData[attr] uses string indexation
+			link.setAttribute(attr, linkData[attr]);
+		});
+		document.head.appendChild(link);
+	});
+}
+
+// Should be the direct child of App, or the most outbound parent
+export default function DSFRWrapper({
+	forWidget,
+	children,
+}: {
+	children: React.ReactNode;
+	forWidget?: boolean;
+}) {
+	startReactDsfr({ defaultColorScheme: 'system' });
+	const cdnUrl = forWidget ? 'http://localhost:4173' : '';
+	loadAssets(cdnUrl);
+	return <>{children}</>;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,47 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
-const URL_CDN = "https://pims-frontend.vercel.app/";
-const URL_LOCALHOST = "http://localhost:4173/";
+const URL_CDN = 'https://pims-frontend.vercel.app/';
+const URL_LOCALHOST = 'http://localhost:4173/';
 const buildLocal = true;
 const URL = buildLocal ? URL_LOCALHOST : URL_CDN;
 
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        widget: "./src/widget.tsx",
-      },
-      output: {
-        entryFileNames: (assetInfo) => {
-          return assetInfo.name === "widget"
-            ? "assets/js/[name].js"
-            : "assets/[name].js";
-        },
-      },
-    },
-  },
-  experimental: {
-    renderBuiltUrl(filename: string) {
-      return URL + filename;
-    },
-  },
+	plugins: [
+		react(),
+		cssInjectedByJsPlugin({
+			styleId: 'vite-injected-css',
+			jsAssetsFilterFunction: function customJsAssetsfilterFunction(
+				outputChunk
+			) {
+				console.log('This is the output Chunk ; ', outputChunk.fileName);
+				return (
+					outputChunk.fileName == 'assets/js/widget.js' ||
+					outputChunk.fileName == 'assets/main.js'
+				);
+			},
+		}),
+	],
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				widget: resolve(__dirname, './src/widget.tsx'),
+			},
+			output: {
+				entryFileNames: (assetInfo) => {
+					return assetInfo.name === 'widget'
+						? 'assets/js/[name].js'
+						: 'assets/[name].js';
+				},
+			},
+		},
+	},
+	experimental: {
+		renderBuiltUrl(filename: string) {
+			return URL + filename;
+		},
+	},
 });
-
-// TUTO
