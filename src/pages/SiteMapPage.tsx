eaca717af596168d8325fca8184e191a036a0a47
David Bretaud <david.bretaud.dev@gmail.com>
2024-09-26 11:08:23 +0200

    style(sitemap): Improve and clean site map page and router

--- src/pages/SiteMapPage.tsx
+++ src/pages/SiteMapPage.tsx
@@ -1,13 +1,20 @@
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import {
 	HREF_ROUTE_LEGAL_INFORMATION,
 	HREF_ROUTE_MAIN,
 	HREF_ROUTE_SITE_MAP,
+	HREF_ROUTE_ACCESSIBILITY,
+	HREF_ROUTE_PRIVACY_POLICY,
 } from '@/providers';
 
 export default function SiteMapPage() {
 	const { t } = useTranslation('site_map_page');
+
+	useEffect(() => {
+		document.title = `${t('title')} | PIMS`;
+	}, [t]);
 
 	const pages = [
 		{
@@ -22,10 +29,19 @@ export default function SiteMapPage() {
 			href: HREF_ROUTE_LEGAL_INFORMATION,
 			i18nkey: 'legal_information',
 		},
+		{
+			href: HREF_ROUTE_ACCESSIBILITY,
+			i18nkey: 'accessibility',
+		},
+		{
+			href: HREF_ROUTE_PRIVACY_POLICY,
+			i18nkey: 'privacy_policy',
+		},
 	];
+
 	return (
-		<section className="pims-layouts__container">
-			<h2>{t('title')}</h2>
+		<section className="pims-components__page">
+			<h1>{t('title')}</h1>
 			<ul className="fr-py-4v">
 				{pages.map((item) => (
 					<li key={`site-map-item-${item.i18nkey}`}>
--- src/providers/AppRoutesProvider.tsx
+++ src/providers/AppRoutesProvider.tsx
@@ -4,12 +4,14 @@ import LegalInformationPage from '@/pages/LegalInformationPage';
 import MainPage from '@/pages/MainPage';
 import SiteMapPage from '@/pages/SiteMapPage';
 import PrivacyPolicyPage from '@/pages/PrivacyPolicyPage';
+import AccessibilityPage from '@/pages/AccessibilityPage';
 
 import {
 	HREF_ROUTE_LEGAL_INFORMATION,
 	HREF_ROUTE_MAIN,
 	HREF_ROUTE_SITE_MAP,
 	HREF_ROUTE_PRIVACY_POLICY,
+	HREF_ROUTE_ACCESSIBILITY,
 } from './constants';
 
 export default function AppRoutesProvider() {
@@ -18,6 +20,7 @@ export default function AppRoutesProvider() {
 			<Route path={HREF_ROUTE_MAIN} element={<MainPage />} />
 			<Route path={HREF_ROUTE_SITE_MAP} element={<SiteMapPage />} />
 			<Route path={HREF_ROUTE_PRIVACY_POLICY} element={<PrivacyPolicyPage />} />
+			<Route path={HREF_ROUTE_ACCESSIBILITY} element={<AccessibilityPage />} />
 			<Route
 				path={HREF_ROUTE_LEGAL_INFORMATION}
 				element={<LegalInformationPage />}
