ed91794173696b3533c3b4795fcd90e0f8ed4171
Léopold Gravier <leopold.gravier@etu.minesparis.psl.eu>
2026-03-19 16:33:17 +0100

    feat(home-page): DSFR-compliant pictograms

--- src/lib/0-home/SectionSteps.tsx
+++ src/lib/0-home/SectionSteps.tsx
@@ -1,4 +1,5 @@
 import { useTranslation, Trans } from 'react-i18next';
+import { useIsDark } from '@codegouvfr/react-dsfr/useIsDark';
 
 import { HOME_SCREEN_STEPS } from './constants';
 import SectionTemplate from './SectionTemplate';
@@ -6,6 +7,8 @@ import SectionTemplate from './SectionTemplate';
 export default function SectionSteps() {
 	const { t } = useTranslation('home_screen');
 	const translationKeyPrefix = 'steps';
+
+	const { isDark } = useIsDark();
 
 	return (
 		<SectionTemplate i18nKey={translationKeyPrefix}>
@@ -15,7 +18,7 @@ export default function SectionSteps() {
 						key={`${translationKeyPrefix}-list-item-${step.id}`}
 						className="pims-home-screen__step-item"
 					>
-						<img src={step.icon} alt={step.i18nKey} width={80} height={80} />
+						<img src={isDark ? step.darkIcon : step.lightIcon} alt={step.i18nKey} width={80} height={80} />
 						<div className="pims-home-screen__step-item-text-container">
 							<p>
 								<b className="fr-mr-1w">{step.id}.</b>
--- src/lib/0-home/constants.ts
+++ src/lib/0-home/constants.ts
@@ -1,11 +1,17 @@
-import stepOne from '@/assets/step-icons/step_one.png';
-import stepTwo from '@/assets/step-icons/step_two.png';
-import stepThree from '@/assets/step-icons/step_three.png';
-import stepFour from '@/assets/step-icons/step_four.png';
-import stepFive from '@/assets/step-icons/step_five.png';
+import step1light from '@/assets/step-icons/step-1-light.svg';
+import step1dark from '@/assets/step-icons/step-1-dark.svg';
+import step2light from '@/assets/step-icons/step-2-light.svg';
+import step2dark from '@/assets/step-icons/step-2-dark.svg';
+import step3light from '@/assets/step-icons/step-3-light.svg';
+import step3dark from '@/assets/step-icons/step-3-dark.svg';
+import step4light from '@/assets/step-icons/step-4-light.svg';
+import step4dark from '@/assets/step-icons/step-4-dark.svg';
+import step5light from '@/assets/step-icons/step-5-light.svg';
+import step5dark from '@/assets/step-icons/step-5-dark.svg';
 
 type HomeScreenStep = {
-	icon: string;
+	lightIcon: string;
+	darkIcon: string;
 	i18nKey: string;
 	id: number;
 };
@@ -14,26 +20,31 @@ export const HOME_SCREEN_STEPS: HomeScreenStep[] = [
 	{
 		id: 1,
 		i18nKey: 'step_one',
-		icon: stepOne,
+		lightIcon: step1light,
+		darkIcon: step1dark,
 	},
 	{
 		id: 2,
 		i18nKey: 'step_two',
-		icon: stepTwo,
+		lightIcon: step2light,
+		darkIcon: step2dark,
 	},
 	{
 		id: 3,
 		i18nKey: 'step_three',
-		icon: stepThree,
+		lightIcon: step3light,
+		darkIcon: step3dark,
 	},
 	{
 		id: 4,
 		i18nKey: 'step_four',
-		icon: stepFour,
+		lightIcon: step4light,
+		darkIcon: step4dark,
 	},
 	{
 		id: 5,
 		i18nKey: 'step_five',
-		icon: stepFive,
+		lightIcon: step5light,
+		darkIcon: step5dark,
 	},
 ];
