a2bc2079c3b6133c35da901b99bc651a2ae3d7c9
Léopold Gravier <leopold.gravier@etu.minesparis.psl.eu>
2026-04-19 12:06:55 +0200

    feat(pdf): Integrated most PDF parameters

--- src/PimsApp.tsx
+++ src/PimsApp.tsx
@@ -3,16 +3,63 @@ import PimsScreen from "./lib/pims/PimsScreen";
 import i18n from "./i18n/i18n";
 import { RadioFrequencies, Risk, UsefulNumbers } from "./providers/AppContextConfig";
 import { RISK_TYPES } from "./lib/1-information/risks/constants";
+import { PimsContent } from "./providers/PdfContext";
 
 
-export function PimsApp() {
-    
-    const params = new URLSearchParams(window.location.search);
-    const title = params.get("title");
-    if (title === null) {
-        throw new Error("Missing argument 'title'");
+function getOptionnalStrParam(params: URLSearchParams, name: string): string|undefined {
+    const value = params.get(name);
+    if (value === null) {
+        return undefined;
     }
 
+    return value;
+}
+
+function getMandatoryStrParam(params: URLSearchParams, name: string): string {
+    const value = params.get(name);
+    if (value === null) {
+        throw new Error(`Missing mandatory argument '${name}'`);
+    }
+
+    return value;
+}
+
+function getMandatoryBoolParam(params: URLSearchParams, name: string): boolean {
+    const value = params.get(name);
+    if (value === null) {
+        throw new Error(`Missing mandatory argument '${name}'`);
+    } else if (value === "true") {
+        return true;
+    } else if (value === "false") {
+        return false;
+    } else {
+        throw new Error(`Invalid format for argument '${name}'. Expecting 'true' or 'false'.`);
+    }
+}
+
+
+function getPimsParams(): PimsContent {
+    // ===== Parse params =====
+    const params = new URLSearchParams(window.location.search);
+
+    const title = getOptionnalStrParam(params, "title");
+    const address = getMandatoryStrParam(params, "address");
+    const comment = getOptionnalStrParam(params, "comment");
+
+    // TODO: Numbers
+    // TODO: Risk list
+
+    const gatheringPlace = getOptionnalStrParam(params, "gathering_place");
+
+    // TODO: Radio frequencies
+
+    const emergencyKitLocation = getMandatoryStrParam(params, "emergency_kit_location");
+    const strimmingObligation = getMandatoryBoolParam(params, "strimming_obligation");
+    const cityName = getMandatoryStrParam(params, "city_name");
+    const iodePastilleElegibility = getMandatoryBoolParam(params, "iode_pastille_elegebility");
+
+
+    // ===== Mockup for not implemented params =====
     const radioFreqs: RadioFrequencies = {
         "franceInter": ["105,1"],
         "ici": ["98,2"],
@@ -59,20 +106,31 @@ export function PimsApp() {
         others: "",
     }
 
+    
+    return {
+        title,
+        address,
+        comment,
+        numbers,
+        riskList: risks,
+        gatheringPlace,
+        emergencyKitLocation,
+        strimmingObligation,
+        iodePastilleElegibility,
+        cityName,
+        radioFreqs,
+    }
+}
+
+
+export function PimsApp() {
+    const content = getPimsParams();
+
+    // ===== Return pims =====
 	return (
         <I18nextProvider i18n={i18n}>
             <PimsScreen
-                title={title}
-                address="15 Rue Monge 38100 Grenoble"
-                comment="Ne pas oublier de couper le gaz en partant"
-                numbers={numbers}
-                riskList={risks}
-                gatheringPlace="Le Parking"
-                radioFreqs={radioFreqs} 
-                emergencyKitLocation="Dans l'escalier de la cave"
-                strimmingObligation={true}
-                cityName="Grenoble"
-                iodePastilleElegibility={true}
+                {...content}
             />
         </I18nextProvider>
 	);
