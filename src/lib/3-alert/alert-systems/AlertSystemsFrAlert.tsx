6b87d47fa5cf860b7912013d3a1499079dbcabaa
Léopold Gravier <leopold.gravier@etu.minesparis.psl.eu>
2026-04-27 22:07:07 +0200

    feat(screen-3): FR-Alert logo for dark-mode

--- src/lib/3-alert/alert-systems/AlertSystemsFrAlert.tsx
+++ src/lib/3-alert/alert-systems/AlertSystemsFrAlert.tsx
@@ -1,14 +1,17 @@
 import { useTranslation, Trans } from 'react-i18next';
+import { useIsDark } from '@codegouvfr/react-dsfr/useIsDark';
 
 import { Container, Subtitle } from '@/components';
-import iconFrAlert from '@/assets/alert-icons/fr_alert.svg';
-import mediaFrAlert from '@/assets/media/sound_fr_alert.mp3';
-
 import FrAlertDiscovery from './FrAlertDiscovery';
 import AudioPlayer from './AudioPlayer';
 
+import iconFrAlertLight from '@/assets/alert-icons/fr_alert_light.svg';
+import iconFrAlertDark from '@/assets/alert-icons/fr_alert_dark.svg';
+import mediaFrAlert from '@/assets/media/sound_fr_alert.mp3';
+
 export default function AlertSystemsFrAlert() {
 	const { t } = useTranslation('alert_screen');
+	const { isDark } = useIsDark();
 
 	const imgSide = 24;
 
@@ -27,7 +30,7 @@ export default function AlertSystemsFrAlert() {
 			<p>{t('alert_systems.fr_alert.simulation')}</p>
 			<AudioPlayer
 				media={mediaFrAlert}
-				logoSrc={iconFrAlert}
+				logoSrc={isDark ? iconFrAlertDark : iconFrAlertLight}
 				logoHeight={imgSide}
 				logoWidth={imgSide}
 				buttonTitle={t('alert_systems.fr_alert.button')}
